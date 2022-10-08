4f5d603ae1e7a90186e2b05aa6bb03517151eafb
deokmoon <deok2moon@gmail.com>
2022-10-08 17:13:29 +0900

    Test: 우테캠 제공 라이브러리 테스트코드 적용 - 예외 처리 라이브러리 적용

--- src/test/java/racingcar/utils/InputCarNamesTest.java
+++ src/test/java/racingcar/utils/InputCarNamesTest.java
@@ -1,17 +1,19 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static racingcar.utils.RacingCarConstants.MSG_CAR_NAME_DUPLICATE_EXCEPTION;
 import static racingcar.utils.RacingCarConstants.MSG_GAME_PARTICIPANT_ILLEGAR_EXCEPTION;
 
-public class InputCarNamesTest {
+public class InputCarNamesTest extends NsTest {
     @Test
     @DisplayName("자동차 이름 입력 테스트")
     void 자동차_이름_입력_테스트() {
@@ -30,28 +32,27 @@ public class InputCarNamesTest {
     @DisplayName("입력받은 이름 중 중복 시 예외처리")
     @Test
     void 이름_중복_체크() {
-        // given
-        String inputCli = "pobi,dobi,pobi";
-
-        // when
-        System.setIn(new ByteArrayInputStream(inputCli.getBytes(StandardCharsets.UTF_8)));
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> InputCarNames.inputCarNames());
-
-        // then
-        assertThat(exception.getMessage()).isEqualTo(MSG_CAR_NAME_DUPLICATE_EXCEPTION);
+        assertSimpleTest(
+                () -> {
+                    runException("pobi,dobi,pobi");
+                    assertThat(output()).contains(MSG_CAR_NAME_DUPLICATE_EXCEPTION);
+                }
+        );
     }
 
     @DisplayName("참가자 인원이 2 미만인 경우 예외처리")
     @Test
     void 참가자_인원_부족_테스트() {
-        // given
-        String inputCli = "pobi";
+        assertSimpleTest(
+                () -> {
+                    runException("pobi");
+                    assertThat(output()).contains(MSG_GAME_PARTICIPANT_ILLEGAR_EXCEPTION);
+                }
+        );
+    }
 
-        // when
-        System.setIn(new ByteArrayInputStream(inputCli.getBytes(StandardCharsets.UTF_8)));
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> InputCarNames.inputCarNames());
-
-        // then
-        assertThat(exception.getMessage()).isEqualTo(MSG_GAME_PARTICIPANT_ILLEGAR_EXCEPTION);
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
--- src/test/java/racingcar/utils/InputMoveCountTest.java
+++ src/test/java/racingcar/utils/InputMoveCountTest.java
@@ -1,18 +1,20 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static racingcar.utils.RacingCarConstants.MSG_MOVE_COUNT_LENGTH_EXCEED_EXCEPTION;
 import static racingcar.utils.RacingCarConstants.MSG_MOVE_MIN_VALUE_EXCEPTION;
 
-public class InputMoveCountTest {
+public class InputMoveCountTest extends NsTest {
     @DisplayName("입력받은 시도할 횟수에 대한 정상 입력 확인")
     @Test
     public void 시도할_횟수_정상_입력_테스트() {
@@ -31,29 +33,23 @@ public class InputMoveCountTest {
     @DisplayName("입력받은 시도할 횟수에 대한 길이 테스트(최대 4)")
     @Test
     public void 입력받은_숫자_4자리_초과_예외발생() {
-        //given
-        String inputCli = "55555";
-
-        //when
-        System.setIn(new ByteArrayInputStream(inputCli.getBytes(StandardCharsets.UTF_8)));
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> InputMoveCount.inputMoveCount());
-
-        // then
-        assertThat(exception.getMessage()).isEqualTo(MSG_MOVE_COUNT_LENGTH_EXCEED_EXCEPTION);
+        assertSimpleTest(
+                () -> {
+                    runException("tobi,venue", "555555");
+                    assertThat(output()).contains(MSG_MOVE_COUNT_LENGTH_EXCEED_EXCEPTION);
+                }
+        );
     }
 
     @DisplayName("입력받은 시도할 횟수에 대한 값 테스트(최소 1)")
     @Test
     public void 입력받은_숫자_1이상_아님_예외발생() {
-        //given
-        String inputCli = "0";
-
-        //when
-        System.setIn(new ByteArrayInputStream(inputCli.getBytes(StandardCharsets.UTF_8)));
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> InputMoveCount.inputMoveCount());
-
-        // then
-        assertThat(exception.getMessage()).isEqualTo(MSG_MOVE_MIN_VALUE_EXCEPTION);
+        assertSimpleTest(
+                () -> {
+                    runException("tobi,venue", "0");
+                    assertThat(output()).contains(MSG_MOVE_MIN_VALUE_EXCEPTION);
+                }
+        );
     }
 
     @DisplayName("숫자가 아닌 다른 문자를 입력 했을 때 예외처리 테스트")
@@ -69,5 +65,10 @@ public class InputMoveCountTest {
         assertThatExceptionOfType(NumberFormatException.class)
                 .isThrownBy(() -> InputMoveCount.inputMoveCount());
 
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
